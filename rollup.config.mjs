b9c14421eaa40211c20ee0cc00716bd665d71be9
pkunszt <pkunszt@gmail.com>
2025-05-13 12:02:21 +0200

    ios config

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -4,7 +4,7 @@ export default {
     {
       file: 'dist/plugin.js',
       format: 'iife',
-      name: 'capacitorCommunityBluetoothLe',
+      name: 'medmonitorBluetoothLe',
       globals: {
         '@capacitor/core': 'capacitorExports',
       },
